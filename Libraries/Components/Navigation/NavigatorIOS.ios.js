64bf2216ceee77379f985265e4fa01a4225a7de2
tony tong <ttt@tttmac-4.attlocal.net>
2015-12-01 16:15:46 -0800

    merged HiQ mods

--- Libraries/Components/Navigation/NavigatorIOS.ios.js
+++ Libraries/Components/Navigation/NavigatorIOS.ios.js
@@ -155,6 +155,7 @@ type Event = Object;
  *  - `resetTo(route)` - Replaces the top item and popToTop
  *  - `popToRoute(route)` - Go back to the item for a particular route object
  *  - `popToTop()` - Go back to the top item
+ *  - `getCurrentRoutes()` - Returns the current list of routes
  *
  * Navigator functions are also available on the NavigatorIOS component:
  *
@@ -339,6 +340,7 @@ var NavigatorIOS = React.createClass({
       resetTo: this.resetTo,
       popToRoute: this.popToRoute,
       popToTop: this.popToTop,
+      getCurrentRoutes: this.getCurrentRoutes,
       navigationContext: this.navigationContext,
     };
     this._emitWillFocus(this.state.routeStack[this.state.observedTopOfStack]);
@@ -625,6 +627,10 @@ var NavigatorIOS = React.createClass({
     }
     this.replaceAtIndex(route, 0);
     this.popToRoute(route);
+  },
+
+  getCurrentRoutes: function(): Array<Route> {
+    return this.state.routeStack;
   },
 
   handleNavigationComplete: function(e: Event) {
